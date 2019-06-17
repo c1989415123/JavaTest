3f000da6210f5d85cbe64f5bb6ce481a842e54a1
c1989415123 <623483204@qq.com>
2019-06-17 16:15:56 +0800

    初次

---
+++ src/main/java/com/baofu/cloud/javatest/Bootstarp.java
@@ -0,0 +1,13 @@
+package com.baofu.cloud.javatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstarp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstarp.class, args);
+    }
+
+}
---
+++ src/main/java/com/baofu/cloud/javatest/controller/HWController.java
@@ -0,0 +1,15 @@
+package com.baofu.cloud.javatest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HWController {
+
+    @RequestMapping("/")
+    public String helloworld() {
+        System.out.println("hello world!");
+        return "github hello world!";
+    }
+
+}
